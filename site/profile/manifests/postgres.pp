c2ccf0a79ad553d8a32b759ee660285229a43e1b
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-14 16:37:31 -0400

    Install postgres

---
+++ site/profile/manifests/postgres.pp
@@ -0,0 +1,15 @@
+class profile::postgress {
+
+  $dbrootpw = secret('postgres/root', {
+    bytes  => 256,
+    method => 'y64',
+  })
+
+  class { '::postgresql::server':
+    postgres_password => file($dbrootpw),
+    data_checksums    => true,
+    encoding          => 'UTF-8',
+    locale            => 'en_US.UTF-8',
+  }
+
+}
