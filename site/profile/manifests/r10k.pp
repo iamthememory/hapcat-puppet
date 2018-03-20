6f1e6118aa34b249dd0b4a16371a2ae35c8e1bb2
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-20 16:21:30 -0400

    Update Puppet configuration with cron

---
+++ site/profile/manifests/r10k.pp
@@ -0,0 +1,8 @@
+class profile::r10k {
+  cron { 'r10k-update':
+    present => 'ensure',
+    command => '/opt/puppetlabs/bin/r10k deploy environment -pv',
+    user    => 'root',
+    minute  => [0, 10, 20, 30, 40, 50],
+  }
+}
