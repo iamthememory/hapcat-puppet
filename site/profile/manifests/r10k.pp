ccd2fe4cd0cb68aeee0cda99fca8afb58d3a385f
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-20 16:22:54 -0400

    Fix typo

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -1,6 +1,6 @@
 class profile::r10k {
   cron { 'r10k-update':
-    present => 'ensure',
+    ensure  => 'present',
     command => '/opt/puppetlabs/bin/r10k deploy environment -pv',
     user    => 'root',
     minute  => [0, 10, 20, 30, 40, 50],
