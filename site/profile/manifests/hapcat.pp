a83966f7554f3cf6cb9f9c5094f2726a49037b53
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-22 13:46:45 -0400

    Update hapcat

--- site/profile/manifests/hapcat.pp
+++ site/profile/manifests/hapcat.pp
@@ -1,6 +1,6 @@
 class profile::hapcat {
   class { '::hapcat':
-    package_pip_url          => 'git+https://github.com/iamthememory/hapcat-backend.git@eeeb36662fc25c4238252b024f2a2871a442ad11',
+    package_pip_url          => 'git+https://github.com/iamthememory/hapcat-backend.git@5042ce28e162304f04a015b81467e63d4444a489',
     package_pip_install_args => '--upgrade',
     package_ensure           => 'latest',
   }
