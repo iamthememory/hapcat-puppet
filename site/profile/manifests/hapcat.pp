9d304b6d081219cb689fc4d8ff34b8ff6749bac7
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-22 14:40:08 -0400

    Try to force pip upgrades

--- site/profile/manifests/hapcat.pp
+++ site/profile/manifests/hapcat.pp
@@ -1,7 +1,7 @@
 class profile::hapcat {
   class { '::hapcat':
     package_pip_url          => 'git+https://github.com/iamthememory/hapcat-backend.git@5042ce28e162304f04a015b81467e63d4444a489',
-    package_pip_install_args => '--upgrade',
+    package_pip_install_args => '--upgrade --force-reinstall --ignore-installed',
     package_ensure           => 'latest',
   }
 }
