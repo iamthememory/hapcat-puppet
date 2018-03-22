3ededbc4a788d15d370458c38275c683b5b2b39d
Alex Corkwell <i.am.the.memory@gmail.com>
2018-03-22 15:20:39 -0400

    Fix syntax error

--- site/profile/manifests/hapcat.pp
+++ site/profile/manifests/hapcat.pp
@@ -1,6 +1,5 @@
 class profile::hapcat {
   class { '::hapcat':
-    package_pip_url          => 'git+https://github.com/iamthememory/hapcat-backend.git'
-    package_pip_install_args => '--upgrade --force-reinstall --ignore-installed',
+    package_pip_url          => 'git+https://github.com/iamthememory/hapcat-backend.git',
   }
 }
