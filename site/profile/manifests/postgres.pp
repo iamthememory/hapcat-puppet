5cd58d1ac5a85240311d2aeee84d2555713fce7b
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-17 16:56:46 -0400

    Create hapcat database

--- site/profile/manifests/postgres.pp
+++ site/profile/manifests/postgres.pp
@@ -1,12 +1,27 @@
 class profile::postgres {
 
   $postgres_pw = hiera('postgres_pw')
+  $hapcatdb = hiera('hapcatdb')
+  $hapcatdbuser = hiera('hapcatdbuser')
+  $hapcatdbpw = hiera('hapcatdbpw')
 
   class { '::postgresql::server':
     postgres_password => $postgres_pw,
     data_checksums    => true,
     encoding          => 'UTF-8',
     locale            => 'en_US.UTF-8',
+  }
+
+  postgresql::server::role { $hapcatdbuser :
+    ensure          => 'present',
+    password_hash   => postgresql_password($hapcatdbuser, $hapcatdbpw),
+    update_password => true,
+    username        => $hapcatdbuser,
+  } ->
+  postgresql::server::database { $hapcatdb :
+    ensure  => 'present',
+    comment => 'Hapcat database',
+    owner    => $hapcatdbuser,
   }
 
 }
