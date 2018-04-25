0c25e7720ff9ec19aedc0b9a77fcd7683ec67958
Alex Corkwell <i.am.the.memory@gmail.com>
2018-04-25 18:29:18 -0400

    Add nginx

--- site/profile/manifests/hapcat.pp
+++ site/profile/manifests/hapcat.pp
@@ -2,4 +2,9 @@ class profile::hapcat {
   class { '::hapcat':
     package_pip_url          => 'git+https://github.com/iamthememory/hapcat-backend.git',
   }
+
+  class { '::nginx':
+    manage_repo    => true,
+    package_source => 'nginx-mainline',
+  }
 }
